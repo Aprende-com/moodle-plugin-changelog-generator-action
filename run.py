06717cc85f5d7cae6a2f628484fd8f77e37d12b1
Alexander <41798528+aholguin508@users.noreply.github.com>
2023-02-05 21:36:13 -0400

    Update run.py

--- run.py
+++ run.py
@@ -46,7 +46,7 @@ def main():
     cmd_log = 'git log'
     print(f"Analizing git repository at: {args.path}")
     repo = Repo(args.path)
-    print("printing chdir", os.chdir("${PLUGIN_LOCAL}"))    
+    print("printing chdir", os.chdir(os.environ['PLUGIN_LOCAL'])) 
     print("printing pwd", os.getcwd())
     print("printing ls command", os.system(cmd_ls))
     print("printing git log command", os.system(cmd_log))
